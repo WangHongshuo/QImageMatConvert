ea253191dc34d4d186b0130a734585dcbfcc2f32
Wang Hongshuo <261454983@qq.com>
2017-11-12 20:38:09 +0800

    Add files via upload

---
+++ mat_mwarray_convert.h
@@ -0,0 +1,13 @@
+﻿#ifndef MAT_MWARRAY_CONVERT_H
+#define MAT_MWARRAY_CONVERT_H
+
+#include "mclmcrrt.h"
+#include "mclcppclass.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+
+mwArray mat2mwArray(const cv::Mat imgSrc);
+cv::Mat mwArray2mat(const mwArray data);
+
+#endif // MAT_MWARRAY_CONVERT_H
