f652794ae534707492e9b981cba3cf43fb23c219
Wang Hongshuo <iswanghongshuo@foxmail.com>
2017-12-16 17:20:22 +0800

    updated mat_qimage_convert

--- mat_qimage_convert.h
+++ mat_qimage_convert.h
@@ -6,7 +6,10 @@
 #include <opencv2/core/core.hpp>
 #include <QImage>
 
-QImage Mat2QImage(cv::Mat &mat);
-cv::Mat QImage2Mat(QImage image);
+QImage Mat2QImage_with_pointer(cv::Mat &mat);
+cv::Mat QImage2Mat_with_pointer(QImage &image);
+
+QImage Mat2QImage_with_data(cv::Mat &mat);
+cv::Mat QImage2Mat_with_data(QImage &image);
 
 #endif // MAT_QIMAGE_CONVERT_H
